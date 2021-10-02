887aac93c9fc842812aba1ad1cbec5957250ade0
Christian <christianvillere@gmail.com>
2021-10-02 14:11:00 -0700

    Create 1st class with two variables

--- mastermind.rb
+++ mastermind.rb
@@ -1,1 +1,32 @@
 # Project to build the mastermind game for Odin Project
+
+=begin
+colors = ["red", "blue", "green", "purple", "orange", "yellow"]
+
+-have computer generate a random list of colors
+-Display number of guesses left
+-prompt user for guess
+-player guesses that list
+-check players guess to make sure it matches format
+---split on commas
+---make all letters downcase
+---check against a regex expression
+---if ok then continue to next step
+-player's guess is added to list of guesses
+-list that includes the guesses that were correct and empty spots for the guesses
+that weren't is returned/shown
+-display number of guesses left
+-prompt user for guess
+-
+=end
+
+# initial game project
+class MasterMind
+  @@colors = ['red', 'blue', 'green', 'purple', 'orange', 'yellow']
+
+  attr_accessor :num_guesses_rem
+
+  def init
+    @num_guesses_rem = num_guesses_rem
+  end
+end
