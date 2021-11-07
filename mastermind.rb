b04613310676872fce063ced145f18f603c41425
Christian <christianvillere@gmail.com>
2021-11-06 18:02:32 -0700

    Add create_hash func to class ExecuteMasterMind

--- mastermind.rb
+++ mastermind.rb
@@ -44,7 +44,7 @@ class MasterMind
   end
 
   def check_guess_format
-    puts 'What is your guess?(ex. red blue green purple)'
+    puts "What is your guess?(ex. red blue green purple). Possible choices are: #{@@colors}"
     player_guess = gets.chomp
     guess_format = /[a-zA-Z]{3,6}\s[a-zA-Z]{3,6}\s[a-zA-Z]{3,6}\s[a-zA-Z]{3,6}/
     while (player_guess =~ guess_format) == nil
@@ -55,25 +55,26 @@ class MasterMind
     player_guess
   end
 
-  def compare_guess_answer(player, computer)
+  def compare_guess_answer(player, computer, hash)
     feedback_array = []
     player.each_with_index do |p, index|
       if player[index] == computer[index]
-        computer[index] == ' '
+        # computer[index] == ' '
         feedback_array.push(p)
       else
         feedback_array.push(' ')
       end
     end
+    puts "Line 68 - feedback array 1st: #{feedback_array}"
     feedback_array
   end
 
-  def check_answer_position(first_comp, computer, player)
+  def check_answer_position(first_comp, computer, player, hash)
     feedback = first_comp
     comp_answer_copy = computer
     player_guess = player
     player_guess.each_with_index do |item, index|
-      if comp_answer_copy.include?(item)
+      if comp_answer_copy.include?(item) 
         index_val = comp_answer_copy.index(item)
         comp_answer_copy[index_val] = ' '
         if feedback[index] == ' '
@@ -81,7 +82,8 @@ class MasterMind
         end
       end
     end
-    p feedback
+    puts "line 85 - feedback array 2nd: #{feedback}"
+    feedback
   end
 
   def provide_feedback(player, computer)
@@ -102,36 +104,53 @@ end
 
 class ExecuteMasterMind < MasterMind
 
+  def create_hash(computer)
+    computer_hash = {}
+    computer.each do |item|
+      if computer_hash.key?(item) == true
+        computer_hash[item] += 1
+      else
+        computer_hash[item] = 1
+      end
+    end
+    computer_hash
+  end
+
   def play_game(computer_response)
     player_guess = check_guess_format
+    puts "line 109 - player guess: #{player_guess}"
+    puts "line 110 - computer response: #{computer_response}"
+
     first_comparison = compare_guess_answer(player_guess, computer_response)
     second_comparison = check_answer_position(first_comparison, computer_response, player_guess)
-    initial_guess_feedback = provide_feedback(second_comparison, computer_response)
+    # initial_guess_feedback = provide_feedback(second_comparison, computer_response)
   end
 
   # You are stuck in an infinite loop when calling this function below
   def continue_game(computer_answer)
-    game_play = play_game(computer_answer)
-    while game_play != computer_answer
-      if game_play == computer_answer
+    game_play = play_game(computer_answer, computer_answer)
+    puts "line 120 - computer answer: #{computer_answer}"
+    while game_play != computer_answer_copy
+      if game_play == computer_answer_copy
         puts 'You have won the game'
         exit
-      elsif game_play != computer_answer && @@max_guesses == 0
-        puts "You Lost!"
+      elsif game_play != computer_answer_copy && @@max_guesses == 0
+        puts "line 127 - You Lost!"
         exit
       else
         @num_guesses_rem = @@max_guesses - 1
-        puts "your number of guesses remaining equals: #{@num_guesses_rem}"
+        puts "line 132 - your number of guesses remaining equals: #{@num_guesses_rem}"
       end
-      game_play = play_game(computer_answer)
+      game_play = play_game(computer_answer, computer_answer_copy) 
     end
   end
+
 end
 
 
 my_game = ExecuteMasterMind.new
 computer_answ = my_game.gener_computer_answer
-# game = my_game.play_game(computer_answ)
+comp_hash = my_game.create_hash(computer_answ)
 my_game.continue_game(computer_answ)
 
 
